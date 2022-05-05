d94bae67ed23ff8a3896485f2c8f40464774f495
alien.detonator <alien.detonator@gmail.com>
2022-05-05 15:17:02 +0200

    fixed crashes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const cors = require('cors');
-require('dotenv').config({'path': '.env'});
+require('dotenv').config({ 'path': '.env' });
 
 const mysql = require("mysql2");
 
@@ -13,99 +13,83 @@ app.use(bodyparser.json());
 
 //adatbázis csatolása
 
-const db = mysql.createConnection(
-{
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_DATABASE,
-    port:process.env.DB_PORT
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
 })
 
 // adatbázis kapcsolat ellenőrzése
 
-db.connect(err=>
-{
+db.connect(err => {
     if (err) return console.log(err, 'Database Error!');
     console.log("database connected.");
 })
 
 //minden adat kiszedése egy táblából
-app.get('/user',(req, res) =>
-{
-    let qr = `SELECT * FROM user`;
+app.get('/user', (req, res) => {
+    let qr = `SELECT * FROM felhasznalo`;
 
-    db.query(qr,(err,result)=>
-    {
-        if(err)
-        {
-            console.log(err,'error!');
+    db.query(qr, (err, result) => {
+        if (err) {
+            return console.log(err, 'error!');
         }
 
-        if(result.length>0)
-        {
-            res.send(
-            {
+        if (result.length > 0) {
+            return res.send({
                 message: 'all user data',
                 data: result
             })
         }
-        else
-        {
-            res.send(
-            {
-                message:'data not found'
-            })
-        }
+        res.send({
+            message: 'data not found'
+        })
+
     })
 })
 
 //egy adat kiszedése
-app.get('/user/:id',(req,res)=>
-{
+app.get('/user/:id', (req, res) => {
     let getID = req.params.id;
-    let qr = `select * from user where id= ${getID}`
-    db.query(qr,(err,result)=>
-    {
-        if(err) console.log(err);
+    let qr = `select * from felhasznalo where id= ${getID}`
+    db.query(qr, (err, result) => {
+        if (err) return console.log(err);
 
-        if(result.length>0)
-        {
-            res.send(
-                {
-                    message: 'get one data',
-                    data: result
-                }
-            )
+        if (result.length > 0) {
+            return res.send({
+                message: 'get one data',
+                data: result
+            })
         }
+        res.send({
+            message: 'data not found'
+        })
     })
 })
 
 //adatok beszúrása
-app.post('/user',(req,res)=>
-{
+app.post('/user', (req, res) => {
     console.log(req.body, 'createData');
 
     let fullName = req.body.fullname;
     let eMail = req.body.email;
     let ph = req.body.phone;
 
-    let qr = `INSERT INTO user (fullname, email, phone) VALUES('${fullName}', '${eMail}', '${ph}')`;
+    let qr = `INSERT INTO felhasznalo (fullname, email, phone) VALUES('${fullName}', '${eMail}', '${ph}')`;
 
-    db.query(qr,(err,result)=>
-    {
-        if(err) console.log(err);
+    db.query(qr, (err, result) => {
+        if (err) return console.log(err);
 
-        res.send(
-        {
-            message:'data inserted'
+        res.send({
+            message: 'data inserted'
         })
     })
 })
 
 //adatok módosítása
-app.put('/user/:id', (req,res)=>
-{
+app.put('/user/:id', (req, res) => {
     console.log(req.body, 'updateData');
 
     let getID = req.params.id;
@@ -114,41 +98,34 @@ app.put('/user/:id', (req,res)=>
     let eMail = req.body.email;
     let ph = req.body.phone;
 
-    let qr = `UPDATE user SET fullname = '${fullName}', email = '${eMail}', phone = '${ph}' WHERE id = '${getID}'`;
+    let qr = `UPDATE felhasznalo SET fullname = '${fullName}', email = '${eMail}', phone = '${ph}' WHERE id = '${getID}'`;
 
-    db.query(qr,(err,result)=>
-    {
-        if (err) console.log(err);
-        
-        res.send(
-        {
-            message:'data updated'
+    db.query(qr, (err, result) => {
+        if (err) return console.log(err);
+
+        res.send({
+            message: 'data updated'
         })
     })
 
 })
 
 //adat törlése
-app.delete('/user/:id', (req,res)=>
-{
+app.delete('/user/:id', (req, res) => {
     let getID = req.params.id;
 
-    let qr = `DELETE FROM user WHERE id = ${getID}`;
+    let qr = `DELETE FROM felhasznalo WHERE id = ${getID}`;
 
-    db.query(qr,(err,result)=>
-    {
-        if(err) console.log(err);
+    db.query(qr, (err, result) => {
+        if (err) return console.log(err);
 
-        res.send(
-        {
-            message:'data Deleted'
+        res.send({
+            message: 'data Deleted'
         })
     })
 })
 
 //szerver futtatása
-app.listen(process.env.PORT,()=>
-{
-    console.log("server running.");
-    
+app.listen(process.env.PORT, () => {
+    console.log(`server is running on http://localhost:${process.env.PORT}`);
 })
