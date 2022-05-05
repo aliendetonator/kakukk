4e2c039569096a077ed92dcf708ad4383379972f
alien.detonator <alien.detonator@gmail.com>
2022-05-05 15:07:27 +0200

    fixed env

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config({'path': '.env'});
 
 const mysql = require("mysql2");
 
